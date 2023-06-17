2b341724072d816856fbca09342addb0a9f66741
LypVScode <l13253618310@163.com>
2023-06-17 17:45:45 +0800

    慕课网课程修改

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     let box = document.getElementsByClassName('box')[0]
     box.addEventListener('click', () => {
+        console.log('aaa')
         alert('这是测试代码')
     })
 }
