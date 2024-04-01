61fb5964d24f3f018374d2045a1fa542bfcf64d6
LypVScode <l13253618310@163.com>
2024-04-01 10:19:16 +0800

    test-demo

--- demo.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    let box = document.getElementsByClassName('box')[0]
-    box.addEventListener('click', () => {
-        console.log('aaa')
-        alert('这是测试代码')
-    })
-}
